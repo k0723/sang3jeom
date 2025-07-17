61383bbf532721ef620bce6711c05a0b85c57dca
k0723 <kimbum7824@gmail.com>
2025-07-17 17:33:39 +0900

    feat : 레디스 기초 설정

---
+++ backend/user-service/src/main/java/com/example/demo/config/RedisConfig.java
@@ -0,0 +1,20 @@
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory(
+            @Value("${spring.redis.host}") String host,
+            @Value("${spring.redis.port}") int port) {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate(
+            RedisConnectionFactory factory) {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(factory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+}
