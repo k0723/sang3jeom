0c0d48f714a49acfc51dcea7391c2c66d29529cd
SeongUk Cho <tjddnr377@naver.com>
2025-07-29 18:17:43 +0900

    chore: remove user-service code before merging to dev

--- backend/user-service/src/main/java/com/example/demo/repository/UserAiGoodsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.domain.UserAiGoodsEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface UserAiGoodsRepository extends JpaRepository<UserAiGoodsEntity, Long> {
-    List<UserAiGoodsEntity> findByUserId(Long userId);
-} 
